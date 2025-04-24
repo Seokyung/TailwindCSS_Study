6940fbcc2bb0f17ced0a6e32d62f031b5809c6d2
Seokyung <xuejd17@gmail.com>
2025-04-24 15:41:51 +0900

    파일 구조 정리 및 페이지별 라우팅

--- src/app/button.tsx
+++
@@ -1,7 +0,0 @@
-const Button = () => {
-  return (
-    <div>
-      <button>Default</button>
-    </div>
-  );
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex flex-col gap-5">
-      <h1>TailwindCSS</h1>
+    <div className="m-4 flex flex-col gap-2">
+      <h1 className="mb-2 text-3xl font-bold">TailwindCSS Study</h1>
+      <ul>
+        <li>
+          <Link
+            href={`/day1`}
+            className="text-lg font-bold text-blue-700 underline"
+          >
+            Day1
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
