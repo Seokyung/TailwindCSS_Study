239178d96cec35c01215185aa0f513572f07a23e
Seokyung <xuejd17@gmail.com>
2025-05-13 16:33:05 +0900

    [11-8] sentry 연결하기

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://17bcf01dafab6801e2ef919af113c24b@o4509314656829440.ingest.de.sentry.io/4509314657878096",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
